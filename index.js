e76b441d829b7f8a545f822635325f1f072f5322
akash verma <akash.verma@habilelabs.io>
2023-01-24 18:16:07 +0530

    Table

---
+++ index.js
@@ -0,0 +1,106 @@
+let a11=prompt("Enter Table1 first No");
+let a12=prompt("Enter Table1 Second No");
+
+
+
+let resultA1=parseInt(a11)+Number(a12);
+let resultS1=Number(a11)-Number(a12);
+let resultM1=Number(a11)*Number(a12);
+let resultD1=Number(a11)/Number(a12);
+resultD1=resultD1.toFixed(2);
+
+
+
+
+let val11=document.getElementById("v11");
+let val12=document.getElementById("v12");
+let val21=document.getElementById("v21");
+let val22=document.getElementById("v22");
+let val31=document.getElementById("v31");
+let val32=document.getElementById("v32");
+let val41=document.getElementById("v41");
+let val42=document.getElementById("v42");
+
+
+let add13=document.getElementById("a13");
+let sub23=document.getElementById("s23");
+let mul33=document.getElementById("m33");
+let div43=document.getElementById("d43");
+
+
+val11.textContent=a11;
+val12.textContent=a12;
+val21.textContent=a11;
+val22.textContent=a12;
+val31.textContent=a11;
+val32.textContent=a12;
+val41.textContent=a11;
+val42.textContent=a12;
+
+add13.textContent=resultA1;
+sub23.textContent=resultS1;
+mul33.textContent=resultM1;
+div43.textContent=resultD1;
+
+
+
+
+let a_11=prompt("Enter Table2 first No");
+let a_12=prompt("Enter Table2 Second No");
+
+
+
+let result_A1=parseInt(a_11)+Number(a_12);
+let result_S1=Number(a_11)-Number(a_12);
+let result_M1=Number(a_11)*Number(a_12);
+let result_D1=Number(a_11)/Number(a_12);
+result_D1=result_D1.toFixed(2);
+
+
+
+
+let val_11=document.getElementById("v_11");
+let val_12=document.getElementById("v_12");
+let val_21=document.getElementById("v_21");
+let val_22=document.getElementById("v_22");
+let val_31=document.getElementById("v_31");
+let val_32=document.getElementById("v_32");
+let val_41=document.getElementById("v_41");
+let val_42=document.getElementById("v_42");
+
+
+let add_13=document.getElementById("a_13");
+let sub_23=document.getElementById("s_23");
+let mul_33=document.getElementById("m_33");
+let div_43=document.getElementById("d_43");
+
+
+val_11.textContent=a_11;
+val_12.textContent=a_12;
+val_21.textContent=a_11;
+val_22.textContent=a_12;
+val_31.textContent=a_11;
+val_32.textContent=a_12;
+val_41.textContent=a_11;
+val_42.textContent=a_12;
+
+add_13.textContent=result_A1;
+sub_23.textContent=result_S1;
+mul_33.textContent=result_M1;
+div_43.textContent=result_D1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
